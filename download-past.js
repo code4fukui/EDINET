a5935ecde590b4a2d26880eb1dd635a67c1ee4e6
Taisuke Fukuno <fukuno@jig.jp>
2022-12-29 12:32:12 +0900

    update data

--- download-past.js
+++ download-past.js
@@ -4,8 +4,8 @@ import { download } from "./download.js";
 // JCN = 法人番号
 
 //const start = new Day("2022-11-17");
-const start = new Day("2022-12-27");
-const end = new Day("2022-12-27");
+const start = new Day("2022-12-28");
+const end = new Day("2022-12-28");
 const filerNames = new Set();
 for (let d = start; !d.isAfter(end); d = d.nextDay()) {
   console.log(d);
