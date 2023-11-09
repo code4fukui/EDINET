6dc056a4ae8adf24c9ba444c7aa9510a6792eca1
Taisuke Fukuno <fukuno@jig.jp>
2023-11-09 10:41:14 +0900

    fix banner

--- banner.js
+++ banner.js
@@ -1,8 +1,8 @@
 const list = [
-  ["", "東証上場企業カラム地図"],
+  ["./", "東証上場企業カラム地図"],
   ["piechart.html", "時価総額円グラフ"],
   ["treemap.html", "差分ツリーマップ"],
 ];
 const path = document.location.pathname;
 console.log(path);
-banners.innerHTML = list.filter(i => !path.endsWith(i[0])).map(i => `<a class="banner" href=${i[0]}>${i[1]}</a>`).join("\n");
+banners.innerHTML = list.filter(i => (i[0] == "./" && !path.endsWith("/")) || !path.endsWith(i[0])).map(i => `<a class="banner" href=${i[0]}>${i[1]}</a>`).join("\n");
